d4dc905de7fdd903764e34b81928d6127add4f1d
Harry <harryzhu45@gmail.com>
2025-02-11 12:49:34 -0800

    Fix entrypoint

--- cmd/debug.go
+++ cmd/debug.go
@@ -1,15 +1,17 @@
 package main
 
 import (
-	handler "grammarhive-backend/api"
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"time"
+
+	handler "grammarhive-backend/api"
 )
 
 func main() {
-	app := handler.NewApp()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -17,13 +19,32 @@ func main() {
 
 	srv := &http.Server{
 		Addr:         ":" + port,
-		Handler:      http.HandlerFunc(handler.Handler(app)),
+		Handler:      http.HandlerFunc(handler.Handler),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 
-	log.Printf("Starting server on port %s", port)
-	if err := srv.ListenAndServe(); err != nil {
-		log.Fatalf("Could not start server: %s", err)
+	go func() {
+		log.Printf("Starting server on port %s", port)
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Server failed: %s", err)
+		}
+	}()
+
+	// Wait for interrupt signal to shutdown the server
+	sigint := make(chan os.Signal, 1)
+	signal.Notify(sigint, os.Interrupt)
+
+	<-sigint // Ensures a block until signal received
+	log.Println("Shutting down server...")
+
+	// Ensures a context with a timeout for the shutdown
+	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdownCancel()
+
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
 	}
+
+	log.Println("Server exited gracefully")
 }
